c67889f525ad2c023cb91d5b84829e980713f5d2
Chris Mather <chrismather21@outlook.com>
2022-09-30 15:12:59 +0100

    Further JS functionality

--- script.js
+++ script.js
@@ -1,109 +1,86 @@
 const gameBoard = document.querySelector('.game-board');
 const gameBoardDivs = gameBoard.children;
-const GUESS_LENGTH = 5;
-let firstGuessArray = [];
+const result = document.querySelector('.result');
+let GUESS_LENGTH = 5;
+let correctLetter = 0;
 let iterationCount = 0;
 
-// GET WORD OF THE DAY
-async function getWord() {
+async function wordMaster() {
   const res = await fetch('https://words.dev-apis.com/word-of-the-day');
   const { word: wordRes } = await res.json();
   const word = wordRes.toUpperCase();
-  const wordParts = word.split('');
-  console.log(wordParts, 'CRAZE');
+  console.log(word);
 
-  function enter() {
-    for (let i = 0; i < 5; i++) {
-      let concatenatedGuessTest = (gameBoardDivs[0].innerHTML +=
-        gameBoardDivs[i].innerHTML);
-    }
-
-    for (let i = 0; i < 5; i++) {
-      let concatGuess = gameBoardDivs[i].innerHTML.toUpperCase().toString();
-      let concatenatedGuess = (gameBoardDivs[0].innerHTML +=
-        gameBoardDivs[i].innerHTML);
-
-      console.log(concatGuess, 'Concat Guess');
-      console.log(concatenatedGuessTest, 'Concatenated Guess');
-
-      if (concatGuess === wordParts[i].toString() && isLetter(concatGuess)) {
+  function guessChecker() {
+    for (let i = 0; i < GUESS_LENGTH; i++) {
+      console.log(word[i]);
+      console.log(gameBoardDivs[i]);
+      if (gameBoardDivs[i].innerHTML.toUpperCase() === word[i]) {
+        console.log('match!');
         gameBoardDivs[i].style.backgroundColor = 'green';
-        console.log(isLetter(concatGuess));
-      } else if (
-        gameBoardDivs[i].innerHTML.concat() ===
-        isLetter(!gameBoardDivs[i].innerHTML.concat())
-      ) {
-        console.log('match');
+        gameBoardDivs[i].style.color = 'white';
+        correctLetter++;
+      } else if (gameBoardDivs[i].innerHTML.toUpperCase() !== word[i]) {
+        gameBoardDivs[i].style.backgroundColor = 'grey';
+        gameBoardDivs[i].style.color = 'white';
       }
     }
+
+    if (correctLetter === 5) {
+      console.log('winner');
+      result.innerHTML = 'YOU WON!';
+    }
+
+    GUESS_LENGTH = GUESS_LENGTH + 5;
   }
 
-  function enterTest() {
-    let concatGuess =
-      (gameBoardDivs[0].innerHTML +=
-      gameBoardDivs[1].innerHTML +=
-      gameBoardDivs[2].innerHTML +=
-      gameBoardDivs[3].innerHTML +=
-        gameBoardDivs[4].innerHTML);
-
-    if (iterationCount < 5) {
+  function enter() {
+    if (iterationCount < GUESS_LENGTH) {
+      console.log('not enough letters');
       return;
-      console.log('not enough letters guessed');
-    } else if (guessCheck(concatGuess)) {
-      console.log('is');
+    } else if (iterationCount === GUESS_LENGTH) {
+      console.log('run guess checker');
+      guessChecker();
     }
   }
 
-  // Check if the guess is a real word
-  //   function guessCheck(value) {
-  //   const response = await fetch('https://words.dev-apis.com/validate-word', {
-  //     method: 'POST',
-  //     body: JSON.stringify({ word: value }),
-  //   });
-  //   const { validWord } = await response.json();
-
-  //   // not valid, mark the word as invalid and return
-  //   if (!validWord) {
-  //     markInvalidWord();
-  //     return;
-  //   }
-  // }
-
-  function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
-  }
-
-  function firstGuess(value) {
-    if (
-      value.keyCode >= 65 &&
-      value.keyCode <= 90 &&
-      GUESS_LENGTH > iterationCount
-    ) {
-      gameBoardDivs[iterationCount].innerHTML = value.key;
-      iterationCount++;
-    } else if (
-      value.keyCode !== 65 &&
-      value.keyCode !== 90 &&
-      value.keyCode !== 13 &&
-      value.keyCode !== 8
-    ) {
-      console.log('non number');
-    } else if (value.keyCode === 13 && iterationCount === 5) {
-      enterTest();
-    } else if (value.keyCode === 8) {
-      console.log('backspace');
+  function backspace() {
+    if (iterationCount + 5 === GUESS_LENGTH) {
+      console.log('no letters to delete');
+    } else {
       gameBoardDivs[iterationCount - 1].innerHTML = '';
       iterationCount--;
     }
   }
 
-  function run() {
+  function input(value) {
+    if (
+      value.keyCode >= 65 &&
+      value.keyCode <= 90 &&
+      GUESS_LENGTH !== iterationCount
+    ) {
+      gameBoardDivs[iterationCount].innerHTML = value.key;
+      iterationCount++;
+    } else if (
+      (value.keyCode < 65 && value.keyCode !== 8 && value.keyCode !== 13) ||
+      value.keyCode > 90
+    ) {
+      console.log('inactive key!', value.keyCode);
+      return;
+    } else if (value.keyCode === 13) {
+      enter();
+    } else if (value.keyCode === 8) {
+      backspace();
+    }
+  }
+
+  function initialise() {
     window.addEventListener('keydown', function (event) {
-      firstGuess(event);
+      input(event);
     });
   }
 
-  run();
+  initialise();
 }
 
-getWord();
+wordMaster();
